52fb3d49f21bdf1aff26f5ff9c6c3355bcd568a4
Fozzik <fozzila@mail.ru>
2026-05-22 14:34:40 +0500

    4
    4

--- ssss.js
+++ ssss.js
@@ -76,15 +76,10 @@
 
           var video = {
             title: element.name,
-            url: Api.account(qualityDefault(qualitys) || element.video, true),
-            url_reserve: data.qualitys_proxy ? Api.account(qualityDefault(data.qualitys_proxy) || element.video_reserve || element.video, true) : false,
+            url: qualityDefault(qualitys) || element.video,
+            url_reserve: data.qualitys_proxy ? qualityDefault(data.qualitys_proxy) || element.video_reserve || element.video : false,
             quality: qualitys
           };
-          // Если URL оказался заглушкой VIP, попробуем взять превью как источник
-          try {
-            if (/vip\.mp4/.test(video.url) && element.preview) video.url = Api.account(element.preview, true);
-            if (video.url_reserve && /vip\.mp4/.test(video.url_reserve) && element.preview) video.url_reserve = Api.account(element.preview, true);
-          } catch (e) {}
           Lampa.Player.play(video);
 
           if (recomends.length) {
@@ -128,15 +123,10 @@
 
         var video = {
           title: element.name,
-          url: Api.account(qualityDefault(element.qualitys) || element.video, true),
-          url_reserve: Api.account(qualityDefault(element.qualitys_proxy) || element.video_reserve || '', true),
+          url: qualityDefault(element.qualitys) || element.video,
+          url_reserve: qualityDefault(element.qualitys_proxy) || element.video_reserve || '',
           quality: element.qualitys
         };
-        // Если итоговый URL оказался заглушкой VIP, используем превью как источник
-        try {
-          if (/vip\.mp4/.test(video.url) && element.preview) video.url = Api.account(element.preview, true);
-          if (video.url_reserve && /vip\.mp4/.test(video.url_reserve) && element.preview) video.url_reserve = Api.account(element.preview, true);
-        } catch (e) {}
         Lampa.Player.play(video);
         Lampa.Player.playlist([video]);
         Lampa.Player.callback(function () {
